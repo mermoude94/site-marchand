303ec1f94c040d070129ad07176021bf322f2d2f
mehdi benmouna <94563180+mermoude94@users.noreply.github.com>
2024-03-29 18:30:31 +0100

    Create Vue_Acceuil.php

---
+++ Vue/Vue_Acceuil.php
@@ -0,0 +1,9 @@
+<center>
+		<div class="Titre">
+			<h1>Les belle montres</h1>
+			<h2>Bonjour et bien venue</h2>
+		</div>
+		<div class="Presentation">
+			Les belle montres et un site destiné a la vente de montre entre particulier.
+		</div>
+</center>
