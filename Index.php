0353a39428c95187cd69d9c81b166fa422a20af2
mehdi benmouna <94563180+mermoude94@users.noreply.github.com>
2024-03-29 18:30:28 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -6,7 +6,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Les Bon PLans</title>
 	<meta charset="utf-8">
 </head>
 <body>
@@ -41,26 +40,26 @@
 		if  (isset($_SESSION['email']))
 		{
 			echo '
-				<h1>Les bon plans</h1>
-				<a href="index.php?page=1">
+				<a href="index.php?page=2">
 					<img src="Vue/Asset/Image/Home.png" height="100" width="100" alt="Page d accueil">
 				</a>
-				<a href="index.php?page=2">
+				<a href="index.php?page=3>
 					<img src="image/client.png" height="100" width="100" alt="Annonce">
 				</a>
-				<a href="index.php?page=3">
+				<a href="index.php?page=4">
 					<img src="image/produit.png" height="100" width="100" alt="Mes annonce">
 				</a>
-				<a href="index.php?page=4">
+				<a href="index.php?page=5">
 					<img src="image/technicien.png" height="100" width="100" alt="Ajouter une annoce">
 				</a>
-				<a href="index.php?page=5">
+				<a href="index.php?page=6">
 					<img src="Vue/Asset/Image/client.png" height="100" width="100" alt="Mon Compte">
 				</a>
-				<a href="index.php?page=7">
+				<a href="index.php?page=8">
 					<img src="Vue/Asset/Image/deconnexion.png" height="100" width="100" alt="Déconnexion">
 				</a>
 				</center>';
+				
 				
 		}
 		if(isset($_GET['page']))
@@ -74,12 +73,14 @@
 		switch ($page)
 		{
 			case 1 : require_once ("index.php"); break;
-			case 2 : require_once ("Gestion_Annonce.php"); break;
-			case 3 : require_once ("Gestion_user_Annonce.php"); break;
-			case 4 : require_once ("Gestion_AjoutAnnonce.php"); break;
-			case 5 : require_once ("Gestion_MonCompte.php"); break;
-			case 6 : require_once ("Gestion_UneAnnonce.php"); break;
-			case 7 : session_destroy();
+			case 2 : require_once ("Gestion_acceuil.php"); break;
+			case 3 : require_once ("Gestion_Annonce.php"); break;
+			case 4 : require_once ("Gestion_user_Annonce.php"); break;
+			case 5 : require_once ("Gestion_AjoutAnnonce.php"); break;
+			case 6 : require_once ("Gestion_MonCompte.php"); break;
+			case 7 : require_once ("Gestion_UneAnnonce.php"); break;
+			case 8 : session_destroy();
+
 			unset($_SESSION['email']);
 			header("Location: index.php?page=1");
 			break;
